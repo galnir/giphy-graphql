937192005ec7c76aeb1744fa94c052faad472b00
Nir <nirnir122@gmail.com>
2019-03-21 15:16:06 +0200

    import the random api layer and add it to the server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import { ApolloServer } from 'apollo-server';
 import { GiphySearchAPI } from './search/search.datasource';
 import { GiphyTrendingAPI } from './trending/trending.datasource';
+import { GiphyRandomAPI } from './random/random.datasource';
 import { searchTypeDefs } from './search/search.typeDefs';
 import { searchResolvers } from './search/search.resolvers';
 import { trendingTypeDefs } from './trending/trending.typeDefs';
 import { trendingResolvers } from './trending/trending.resolvers';
+import { randomTypeDefs } from './random/random.typeDefs';
+import { randomResolvers } from './random/random.resolvers';
 import merge from 'lodash.merge';
 
-const typeDefs = [searchTypeDefs, trendingTypeDefs];
+const typeDefs = [searchTypeDefs, trendingTypeDefs, randomTypeDefs];
 
-const resolvers = merge({}, searchResolvers, trendingResolvers);
+const resolvers = merge(
+  {},
+  searchResolvers,
+  trendingResolvers,
+  randomResolvers
+);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     GiphySearchAPI: new GiphySearchAPI(),
-    GiphyTrendingAPI: new GiphyTrendingAPI()
+    GiphyTrendingAPI: new GiphyTrendingAPI(),
+    GiphyRandomAPI: new GiphyRandomAPI()
   })
 });
 
