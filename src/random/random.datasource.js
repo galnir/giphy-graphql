8f5908964b28ffd396747f71b2653de822dbf132
Nir <nirnir122@gmail.com>
2019-03-21 14:55:21 +0200

    write a datasource for the random api endpoint

---
+++ src/random/random.datasource.js
@@ -0,0 +1,17 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+import { GIPHY_KEY } from '../../config.json';
+
+export class GiphyRandomAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = `http://api.giphy.com/v1/gifs/`;
+  }
+
+  async getRandomGif(query) {
+    const data = await this.get('random', {
+      api_key: GIPHY_KEY,
+      tag: query
+    });
+    return data.data;
+  }
+}
