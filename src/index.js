e6ddc9bbdeacb833b7255b28adbae604b11ff963
Nir <nirnir122@gmail.com>
2019-03-22 20:03:44 +0200

    export the translate layer to the server

--- src/index.js
+++ src/index.js
@@ -2,21 +2,30 @@ import { ApolloServer } from 'apollo-server';
 import { GiphySearchAPI } from './search/search.datasource';
 import { GiphyTrendingAPI } from './trending/trending.datasource';
 import { GiphyRandomAPI } from './random/random.datasource';
+import { GiphyTranslateAPI } from './translate/translate.datasource';
 import { searchTypeDefs } from './search/search.typeDefs';
 import { searchResolvers } from './search/search.resolvers';
 import { trendingTypeDefs } from './trending/trending.typeDefs';
 import { trendingResolvers } from './trending/trending.resolvers';
 import { randomTypeDefs } from './random/random.typeDefs';
 import { randomResolvers } from './random/random.resolvers';
+import { translateTypeDefs } from './translate/translate.typeDefs';
+import { translateResolvers } from './translate/translate.resolvers';
 import merge from 'lodash.merge';
 
-const typeDefs = [searchTypeDefs, trendingTypeDefs, randomTypeDefs];
+const typeDefs = [
+  searchTypeDefs,
+  trendingTypeDefs,
+  randomTypeDefs,
+  translateTypeDefs
+];
 
 const resolvers = merge(
   {},
   searchResolvers,
   trendingResolvers,
-  randomResolvers
+  randomResolvers,
+  translateResolvers
 );
 
 const server = new ApolloServer({
@@ -25,7 +34,8 @@ const server = new ApolloServer({
   dataSources: () => ({
     GiphySearchAPI: new GiphySearchAPI(),
     GiphyTrendingAPI: new GiphyTrendingAPI(),
-    GiphyRandomAPI: new GiphyRandomAPI()
+    GiphyRandomAPI: new GiphyRandomAPI(),
+    GiphyTranslateAPI: new GiphyTranslateAPI()
   })
 });
 
