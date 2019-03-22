fee7bc25a02735cc1fcb8da1ef072c309e745fe0
Nir <nirnir122@gmail.com>
2019-03-22 20:00:22 +0200

    write datasource for the translate endpoint

---
+++ src/translate/translate.datasource.js
@@ -0,0 +1,17 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+import { GIPHY_KEY } from '../../config.json';
+
+export class GiphyTranslateAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = `http://api.giphy.com/v1/gifs/`;
+  }
+
+  async getTranslateGif(query) {
+    const data = await this.get('translate', {
+      api_key: GIPHY_KEY,
+      s: query
+    });
+    return data.data;
+  }
+}
