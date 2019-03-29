0cb441d5f454e6a3c3158d11b58ce5a592c3c148
Nir <nirnir122@gmail.com>
2019-03-29 17:51:08 +0300

    export the gif_id graphQL layer to the server

--- src/gifs/index.js
+++ src/gifs/index.js
@@ -6,13 +6,16 @@ import { gifRandomTypeDefs } from './random/random.typeDefs';
 import { gifRandomResolvers } from './random/random.resolvers';
 import { gifTranslateTypeDefs } from './translate/translate.typeDefs';
 import { gifTranslateResolvers } from './translate/translate.resolvers';
+import { gifIdTypeDefs } from './gif_id/gif_id.typeDefs';
+import { gifIdResolvers } from './gif_id/gif_id.resolvers';
 import merge from 'lodash.merge';
 
 export const GifsTypeDefs = [
   gifSearchTypeDefs,
   gifTrendingTypeDefs,
   gifRandomTypeDefs,
-  gifTranslateTypeDefs
+  gifTranslateTypeDefs,
+  gifIdTypeDefs
 ];
 
 export const GifResolvers = merge(
@@ -20,5 +23,6 @@ export const GifResolvers = merge(
   gifSearchResolvers,
   gifTrendingResolvers,
   gifRandomResolvers,
-  gifTranslateResolvers
+  gifTranslateResolvers,
+  gifIdResolvers
 );
--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ import { GifsSearchAPI } from './gifs/search/search.datasource';
 import { GifsTrendingAPI } from './gifs/trending/trending.datasource';
 import { GifsRandomAPI } from './gifs/random/random.datasource';
 import { GifsTranslateAPI } from './gifs/translate/translate.datasource';
+import { GifsIdAPI } from './gifs/gif_id/gif_id.datasource';
 import { GifsTypeDefs } from './gifs/index';
 import { GifResolvers } from './gifs/index';
-import { StickersSearchAPI } from './stickers/search/search.datasource';
 import { stickerTypeDefs } from './stickers/index';
 import { stickerResolvers } from './stickers/index';
+import { StickersSearchAPI } from './stickers/search/search.datasource';
 import { StickersTrendingAPI } from './stickers/trending/trending.datasource';
 import { StickersRandomAPI } from './stickers/random/random.datasource';
 import { StickersTranslateAPI } from './stickers/translate/translate.datasource';
@@ -24,6 +25,7 @@ const server = new ApolloServer({
     GifsTrendingAPI: new GifsTrendingAPI(),
     GifsRandomAPI: new GifsRandomAPI(),
     GifsTranslateAPI: new GifsTranslateAPI(),
+    GifsIdAPI: new GifsIdAPI(),
     StickersSearchAPI: new StickersSearchAPI(),
     StickersTrendingAPI: new StickersTrendingAPI(),
     StickersRandomAPI: new StickersRandomAPI(),
