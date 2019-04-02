5922df850ab68815b4499f4d9d2f0633b378be75
Nir <nirnir122@gmail.com>
2019-04-02 18:31:39 +0300

    make sticker fields that should be returned required

--- src/stickers/random/random.typeDefs.js
+++ src/stickers/random/random.typeDefs.js
@@ -2,48 +2,48 @@ import { gql } from 'apollo-server';
 
 export const stickerRandomTypeDefs = gql`
   type RandomSticker {
-    type: String
+    type: String!
     id: String!
-    slug: String
+    slug: String!
     url: String!
-    bitly_gif_url: String
-    bitly_url: String
-    embed_url: String
-    username: String
-    source: String
-    content_url: String
-    source_tld: String
-    source_post_url: String
-    is_sticker: Int
-    import_datetime: String
-    trending_datetime: String
+    bitly_gif_url: String!
+    bitly_url: String!
+    embed_url: String!
+    username: String!
+    source: String!
+    content_url: String!
+    source_tld: String!
+    source_post_url: String!
+    is_sticker: Int!
+    import_datetime: String!
+    trending_datetime: String!
     user: User
     images: Image
-    title: String
-    image_original_url: String
-    image_url: String
-    image_mp4_url: String
-    image_frames: String
-    image_width: String
-    image_height: String
-    fixed_height_downsampled_url: String
-    fixed_height_downsampled_width: String
-    fixed_height_downsampled_height: String
-    fixed_width_downsampled_url: String
-    fixed_width_downsampled_width: String
-    fixed_width_downsampled_height: String
-    fixed_height_small_url: String
-    fixed_height_small_still_url: String
-    fixed_height_small_width: String
-    fixed_height_small_height: String
-    fixed_width_small_url: String
-    fixed_width_small_still_url: String
-    fixed_width_small_width: String
-    fixed_width_small_height: String
-    caption: String
+    title: String!
+    image_original_url: String!
+    image_url: String!
+    image_mp4_url: String!
+    image_frames: String!
+    image_width: String!
+    image_height: String!
+    fixed_height_downsampled_url: String!
+    fixed_height_downsampled_width: String!
+    fixed_height_downsampled_height: String!
+    fixed_width_downsampled_url: String!
+    fixed_width_downsampled_width: String!
+    fixed_width_downsampled_height: String!
+    fixed_height_small_url: String!
+    fixed_height_small_still_url: String!
+    fixed_height_small_width: String!
+    fixed_height_small_height: String!
+    fixed_width_small_url: String!
+    fixed_width_small_still_url: String!
+    fixed_width_small_width: String!
+    fixed_width_small_height: String!
+    caption: String!
   }
 
   extend type Query {
-    randomSticker(tag: String, rating: String): RandomSticker
+    randomSticker(tag: String, rating: String): RandomSticker!
   }
 `;
--- src/stickers/search/search.typeDefs.js
+++ src/stickers/search/search.typeDefs.js
@@ -2,36 +2,36 @@ import { gql } from 'apollo-server';
 
 export const stickerSearchTypeDefs = gql`
   type Sticker {
-    type: String
-    id: String
-    slug: String
-    url: String
-    title: String
-    bitly_gif_url: String
-    bitly_url: String
-    embed_url: String
-    username: String
-    source: String
-    rating: String
-    content_url: String
-    source_tld: String
-    source_post_url: String
-    is_sticker: Int
-    import_datetime: String
-    trending_datetime: String
+    type: String!
+    id: String!
+    slug: String!
+    url: String!
+    title: String!
+    bitly_gif_url: String!
+    bitly_url: String!
+    embed_url: String!
+    username: String!
+    source: String!
+    rating: String!
+    content_url: String!
+    source_tld: String!
+    source_post_url: String!
+    is_sticker: Int!
+    import_datetime: String!
+    trending_datetime: String!
     user: User # Some gifs do not have a user
     images: Image
-    _score: Int
+    _score: Int!
   }
 
   extend type Query {
-    sticker(query: String!, offset: Int, rating: String, lang: String): Sticker
+    sticker(query: String!, offset: Int, rating: String, lang: String): Sticker!
     stickers(
       query: String!
       limit: Int
       offset: Int
       rating: String
       lang: String
-    ): [Sticker]
+    ): [Sticker]!
   }
 `;
