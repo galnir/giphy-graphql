7c250a08118aaece62fe7a48e4ee7bce8f0094db
Nir <nirnir122@gmail.com>
2019-04-05 16:15:22 +0300

    change toBe to toEqual, because toBe is used for primitive types like String and Int, whereas toEqual is used mainly for values, which is what the test for

--- src/__tests__/giphy.test.js
+++ src/__tests__/giphy.test.js
@@ -1,6 +1,19 @@
 require('dotenv').config(); // enabling the dotenv package
 const fetch = require('node-fetch');
 
+/*it('should fetch gifs given a query', async () => {
+  await fetch(
+    `http://api.giphy.com/v1/gifs/search?api_key=${
+      process.env.GIPHY_KEY
+    }&q=dog&limit=5`
+  )
+    .then(res => res.json())
+    .then(json => {
+      //expect(json.data.is_sticker).toEqual(0); // 0 means the response is a gif
+      expect(json.pagination.count).toBe(5); // test if limit parameter works
+    });
+  });
+*/
 it('fetches a gif given the id', async () => {
   await fetch(
     `http://api.giphy.com/v1/gifs/xTiQyqwwF7R3bwm2Ag?api_key=${
@@ -9,8 +22,8 @@ it('fetches a gif given the id', async () => {
   )
     .then(res => res.json())
     .then(json => {
-      expect(json.data.id).toBe('xTiQyqwwF7R3bwm2Ag'); // if id from response is equal to input id
-      expect(json.data.is_sticker).toBe(0); // 0 means the response is a gif
+      expect(json.data.id).toEqual('xTiQyqwwF7R3bwm2Ag'); // if id from response is equal to input id
+      expect(json.data.is_sticker).toEqual(0); // 0 means the response is a gif
     });
 });
 
@@ -20,6 +33,6 @@ it('should fetch a sticker', async () => {
   )
     .then(res => res.json())
     .then(json => {
-      expect(json.data.is_sticker).toBe(1); // 1 means the response is a sticker
+      expect(json.data.is_sticker).toEqual(1); // 1 means the response is a sticker
     });
 });
