16dd16a0ad5f49df9d8e566d943642dd55592361
Nir <nirnir122@gmail.com>
2019-04-09 11:14:33 +0300

    update the Query return type name in all of the endpoints that need updating

--- src/gifs/gif_id/gif_id.typeDefs.js
+++ src/gifs/gif_id/gif_id.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from 'apollo-server';
 
 export const gifIdTypeDefs = gql`
   extend type Query {
-    gifByID(id: String!): Gif!
+    gifByID(id: String!): GifData!
   }
 `;
--- src/gifs/gifs_id/gifs_id.typeDefs.js
+++ src/gifs/gifs_id/gifs_id.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from 'apollo-server';
 
 export const gifsIdTypeDefs = gql`
   extend type Query {
-    gifsByID(ids: String!): [Gif!]
+    gifsByID(ids: String!): [GifsData!]
   }
 `;
