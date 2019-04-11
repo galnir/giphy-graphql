ac2aa1ca6527c03d4592a98fb4e1e8b9799582f7
Nir <nirnir122@gmail.com>
2019-04-11 21:27:50 +0300

    make the gifs/translate work with the new GifData type and the pagination object

--- src/gifs/translate/translate.datasource.js
+++ src/gifs/translate/translate.datasource.js
@@ -12,6 +12,6 @@ export class GifsTranslateAPI extends RESTDataSource {
       s,
       weirdness
     });
-    return data.data;
+    return data;
   }
 }
--- src/gifs/translate/translate.resolvers.js
+++ src/gifs/translate/translate.resolvers.js
@@ -1,4 +1,12 @@
 export const gifTranslateResolvers = {
+  GifData: {
+    gifData(data) {
+      return data.data;
+    },
+    pagination() {
+      return null; // There is no pagination in translate data
+    }
+  },
   Query: {
     translateGif: (root, { s, weirdness }, { dataSources }) =>
       dataSources.GifsTranslateAPI.getTranslateGif(s, weirdness)
