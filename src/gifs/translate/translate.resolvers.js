f08fb6cf678d454a14bf80883c7a1ad7bf9f71cc
Nir <nirnir122@gmail.com>
2019-04-22 15:53:14 +0300

    remove pagination field from GifData resolver, because it does not exist in GifData anymore

--- src/gifs/translate/translate.resolvers.js
+++ src/gifs/translate/translate.resolvers.js
@@ -1,9 +1,4 @@
 export const gifTranslateResolvers = {
-  GifData: {
-    pagination() {
-      return null; // There is no pagination in translate data
-    }
-  },
   Query: {
     translateGif: (root, { s, weirdness }, { dataSources }) =>
       dataSources.GifsTranslateAPI.getTranslateGif(s, weirdness)
