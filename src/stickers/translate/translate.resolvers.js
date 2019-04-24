60251f49789d9a82d3573ac0988c1e8b9ff101e1
Nir <nirnir122@gmail.com>
2019-04-24 20:42:52 +0300

    remove translate resolvers because they conflict with search resolvers and are not needed

--- src/stickers/translate/translate.resolvers.js
+++ src/stickers/translate/translate.resolvers.js
@@ -1,12 +1,4 @@
 export const stickerTranslateResolvers = {
-  StickerData: {
-    stickerData(data) {
-      return data.data;
-    },
-    pagination() {
-      return null; // There is no pagination object in translate data
-    }
-  },
   Query: {
     translateSticker: (root, { query }, { dataSources }) =>
       dataSources.StickersTranslateAPI.getTranslateSticker(query)
